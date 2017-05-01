7d722d73164874e6dcbf243e3244d002e8c5f742
Roman <fireball222@mail.ru>
2017-05-01 12:54:53 +0300

    core project and tests

---
+++ src/DotNetRetries/Retry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RRTools.DotNetRetries
+{
+    public static class Retry
+    {
+        public static void While<TException>(Action action, int maxAttempts, TimeSpan delay)
+            where TException : Exception
+        {
+            while (true)
+            {
+                try
+                {
+                    action();
+                    break;
+                }
+                catch (TException)
+                {
+                    Task.Delay(delay).GetAwaiter().GetResult();
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ src/UnitTests/RetryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RRTools.DotNetRetries;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RetryTests
+    {
+        [Fact]
+        public void CanJustExecuteActionWithoutException()
+        {
+            // Arrange
+            var run = false;
+
+            // Act
+            Retry.While<Exception>(() => run = true, 1, TimeSpan.Zero);
+            
+            // Assert
+            Assert.True(run);
+        }
+    }
+}
